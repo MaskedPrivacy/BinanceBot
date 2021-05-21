64cd3f7f813d2b763ef005dff34093c525c459b1
Masked Privacy Lead Developer <79077089+MaskedPrivacy@users.noreply.github.com>
2021-05-21 17:54:31 -0400

    Add files via upload

---
+++ BotBase.cs
@@ -0,0 +1,118 @@
+﻿using BinanceExchange.API.Client;
+using BinanceExchange.API.Market;
+using BinanceExchange.API.Models.Response;
+using Binancian.Framework;
+using Binancian.Trading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binancian.Bots
+{
+    public enum BotTypes
+    {
+        GRID,
+        LEVERAGE_GRID,
+        INFINITE_GRIDS,
+        REVERSE_GRID,
+        REVERSE_LEVERAGE_GRID 
+    };
+
+    public class BotBase
+    {
+        //Provide your configuration and keys here, this allows the client to function as expected.
+        string apiKey = "xxx";
+        string secretKey = "yyy";
+
+        public BotBase()
+        {
+            //set up vars later
+            //Initialise the general client client with config
+            this.client = new BinanceClient(new ClientConfiguration()
+            {
+                ApiKey = apiKey,
+                SecretKey = secretKey,
+            });
+        }
+
+        public BotBase(BotTypes bType, string symbolPair,  double AmountToInvest, int DesiredGrids, double PriceLowest, double PriceHighest)
+        {
+            this.Type = bType;
+            this.BotPair = symbolPair;
+            this.InvestmentTotal = AmountToInvest;
+            this.nGridlines = DesiredGrids;
+            this.PriceLowest = PriceLowest;
+            this.PriceHighest = PriceHighest;
+
+            //Initialise the general client client with config
+            this.client = new BinanceClient(new ClientConfiguration()
+            {
+                ApiKey = apiKey,
+                SecretKey = secretKey,
+            });
+        }
+
+        public async void GetTickerPrice(string Symbol)
+        {
+            //Initialise the general client client with config
+            this.client = new BinanceClient(new ClientConfiguration()
+            {
+                ApiKey = apiKey,
+                SecretKey = secretKey,
+            });
+
+            SymbolPriceChangeTickerResponse a = await client.GetDailyTicker(Symbol);
+
+            Console.WriteLine(a.LastPrice);
+        }
+
+        public bool DoesPriceHaveOrder(decimal price, string symbol)
+        {
+            for(int i = 0; i < OrderList.OrderList.Count; i++)
+            {
+                if (OrderList.OrderList[i].Symbol == symbol && OrderList.OrderList[i].price == price)
+                    return true;
+            }
+
+            return false;
+        }
+
+        virtual public BotTypes Type { get; set; }
+        
+        public string BotPair;
+        
+        public int nGridlines { get; set; } //we must calculate whether user can afford n Grids when starting bot
+
+        public double InvestmentTotal { get; set; } //used for calculations for grid lines based on higher + lower price
+
+        public double ProfitTaken { get; set; } //PNL
+
+        public double ProfitToTake { get; set; } //UPNL
+
+        public double PriceLowest { get; set; }
+        public double PriceMidrange { get; set; }
+        public double PriceHighest { get; set; }
+
+        public double StopLoss { get; set; }
+        public double EndAtPrice { get; set; }
+
+        public double AnnualizedEstimate { get; set; }
+
+        public BinanceClient client { get; set; }
+
+        public Wallet UserWallet { get; set; }
+
+        public Grids GridContainer { get; set; }
+
+        public Dictionary<string, decimal> TickerPrices { get; set; }
+
+        public Orders OrderList { get; set; }
+
+        public static string APIKey;
+        public static string SecretKey;
+
+        public bool IsBotting { get; set; }
+    }
+}
---
+++ GridTrading.cs
@@ -0,0 +1,111 @@
+﻿
+using BinanceExchange.API.Client;
+using Binancian.Framework;
+using CryptoExchange.Net.ExchangeInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Binancian.Trading;
+
+namespace Binancian.Bots
+{
+    public class GridTrading : BotBase
+    {
+        public BotTypes type;
+        public string pair;
+        public decimal investmentAmnt;
+        public int nGrids;
+        public decimal lowest;
+        public decimal highest;
+        private Thread botTrd;
+
+        public GridTrading(BotTypes bType, string symbolPair, decimal AmountToInvest, int DesiredGrids, decimal PriceLowest, decimal PriceHighest)
+        {
+            type = bType;
+            pair = symbolPair;
+            investmentAmnt = AmountToInvest;
+            nGrids = DesiredGrids;
+            lowest = PriceLowest;
+            highest = PriceHighest;
+            
+        }
+
+        public async void InitializeGrids(BinanceClient client, string symbol)
+        {
+            //Get current asset price
+            var priceData = await client.GetDailyTicker(symbol);
+            decimal currentPrice = priceData.LastPrice;
+            this.pair = symbol;
+        
+            Console.WriteLine("Init Grids... Starting Price: {0}", priceData.LastPrice);
+
+            switch (this.type)
+            {
+                case BotTypes.GRID:
+                    
+                    decimal lastPriceDec = Convert.ToDecimal(priceData.LastPrice);
+                    GridContainer = new Grids();
+                    GridContainer.GridList = GridContainer.FillGridList(50, 700.00M, 500.00M, pair, lastPriceDec, 400, BinanceExchange.API.Enums.OrderSide.Buy);
+
+                    //create new thread for cycling
+                    botTrd = new Thread(new ThreadStart(this.CycleAsync));
+                    botTrd.IsBackground = true;
+                    botTrd.Start();
+                    break;
+
+                case BotTypes.REVERSE_GRID:
+                    break;
+
+            };
+        }
+    
+        
+
+        public async void CycleAsync() //!remember concurrency issues!
+        {
+            Console.WriteLine("Starting bot for {0}", pair);
+            this.IsBotting = true;
+            decimal currentPrice;
+
+            while (IsBotting)
+            {
+                var priceData = await client.GetDailyTicker(this.pair);
+                currentPrice = priceData.LastPrice; //Get Price
+                Console.WriteLine("Current Price: {0}", currentPrice);
+
+                this.OrderList = new Orders();
+                this.OrderList.GetOpenOrders("BNBUSDT", client);
+                this.OrderList.UpdateFilledOrders(client, "BNBUSDT");
+
+                foreach (Orders.Order ord in this.OrderList)
+                {
+                    Console.WriteLine("Found order: {0}, {1}, {2}, {3}", ord.id, ord.Symbol, ord.price, ord.isMarket);
+           
+                    if((currentPrice >= ord.price) && (ord.Symbol == this.pair)) //this will hit for all orders existing under our price
+                    {
+                          if (OrderList.WasOrderFilled(client, this.pair, ord.id))
+                          {
+                            Console.WriteLine("Order {0} was filled: {1} units.", ord.id, ord.amount);
+
+                              if(ord.Side == BinanceExchange.API.Enums.OrderSide.Sell) //sell tx
+                              {
+
+                              }
+                              else //buy tx
+                              {
+
+                              }
+                           }
+                    }
+                }
+
+                Thread.Sleep(1500);
+            }
+
+        }
+    
+    }
+}
